4c375a3863bdd3231af3a417856c69a4944dd150
piftai <piftaiwork@gmail.com>
2024-08-14 10:39:36 +0300

    package tasks

---
+++ tasks/task.go
@@ -0,0 +1,11 @@
+package tasks
+
+import "time"
+
+type Task struct {
+	ID int `json:"id"`
+	Description string `json:"description"`
+	Status string `"json:"status"`
+	CreatedAt time.Time `json:"CreatedAt"`
+	UpdatedAt time.Time `json:"UpdatedAt"`
+}
---
+++ tasks/task_manager.go
@@ -0,0 +1,51 @@
+package tasks
+
+import (
+	"encoding/json"
+	"os"
+	"time"
+)
+
+type TaskManager struct {
+	tasks []Task
+	filePath string
+}
+
+func NewTaskManager(filePath string) (*TaskManager, error) {
+	tm := &TaskManager{filePath: filePath}
+	err := tm.loadTasks()
+	return tm, err
+}
+
+func (tm *TaskManager) loadTasks() error {
+	if _, err := os.Stat(tm.filePath); os.IsNotExist(err) {
+		tm.tasks = []Task{}
+		return nil
+	}
+	data, err := os.ReadFile(tm.filePath)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(data, &tm.tasks)
+}
+
+func (tm *TaskManager) saveTasks() error {
+	data, err := json.MarshalIndent(tm.tasks, "", " ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(tm.filePath, data, 0644)
+}
+
+func (tm *TaskManager) AddTask(description string) error {
+	task := Task {
+		ID: len(tm.tasks) + 1,
+		Description: description,
+		Status: "todo",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	tm.tasks = append(tm.tasks, task)
+	return tm.saveTasks()
+}
+
