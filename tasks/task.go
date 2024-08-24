1cb81cbec978aec52dacfaf2edd8766e77b30647
piftai <piftaiwork@gmail.com>
2024-08-24 03:38:24 +0300

    delete useless func from struct

--- tasks/task.go
+++ tasks/task.go
@@ -3,14 +3,9 @@ package tasks
 import "time"
 
 type Task struct {
-	ID int `json:"id"`
-	Description string `json:"description"`
-	Status string `"json:"status"`
-	CreatedAt time.Time `json:"CreatedAt"`
-	UpdatedAt time.Time `json:"UpdatedAt"`
-}
-
-func (a *Task) Update(description string) *Task {
-	a.Description = description
-	return a
+	ID          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      string    `"json:"status"`
+	CreatedAt   time.Time `json:"CreatedAt"`
+	UpdatedAt   time.Time `json:"UpdatedAt"`
 }
