b7fea0bd6d828edb3f0d55bd71fb49fdf1a3c7ca
piftai <piftaiwork@gmail.com>
2024-08-31 01:26:44 +0300

    shit

--- tasks/task.go
+++ tasks/task.go
@@ -3,9 +3,9 @@ package tasks
 import "time"
 
 type Task struct {
-	ID          int       `json:"id"`
+	ID          uint8     `json:"id"`
 	Description string    `json:"description"`
-	Status      string    `"json:"status"`
+	Status      string    `json:"status"`
 	CreatedAt   time.Time `json:"CreatedAt"`
 	UpdatedAt   time.Time `json:"UpdatedAt"`
 }
--- tasks/task_manager.go
+++ tasks/task_manager.go
@@ -1,99 +1,15 @@
 package tasks
 
-import (
-	"encoding/json"
-	"os"
-	"time"
-)
-
 type TaskManager struct {
 	tasks    []Task
 	filePath string
 }
 
-// constructor of TaskManager structure
-func NewTaskManager(filePath string) (*TaskManager, error) {
-	tm := &TaskManager{filePath: filePath}
-	err := tm.loadTasks()
-	return tm, err
+func NewTaskManager(filepath string) (*TaskManager, error) {
+	tm := &TaskManager{filePath: filepath}
+
 }
 
-// func that creates a new file if its not exist by path of tm.filePath
 func (tm *TaskManager) loadTasks() error {
-	if _, err := os.Stat(tm.filePath); os.IsNotExist(err) {
-		tm.tasks = []Task{}
-		return nil
-	}
-	data, err := os.ReadFile(tm.filePath)
-	if err != nil {
-		return err
-	}
-	return json.Unmarshal(data, &tm.tasks)
-}
 
-// this func should be written at end of every func that communicate with tm object.
-// func saving changing
-func (tm *TaskManager) saveTasks() error {
-	data, err := json.MarshalIndent(tm.tasks, "", " ")
-	if err != nil {
-		return err
-	}
-	return os.WriteFile(tm.filePath, data, 0644)
 }
-
-// added a task into file
-func (tm *TaskManager) AddTask(description string) error {
-	task := Task{
-		ID:          len(tm.tasks) + 1, // there a existing a bug when you can create two tasks with same id
-		Description: description,
-		Status:      "todo",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
-	tm.tasks = append(tm.tasks, task)
-	return tm.saveTasks()
-}
-
-// return a file tasks.json
-func (tm *TaskManager) ListTasks() ([]byte, error) {
-	return os.ReadFile(tm.filePath)
-}
-
-// return updated task by id and new description
-func (tm *TaskManager) UpdateTask(taskID int, description string) *Task {
-	for i := range tm.tasks {
-		if tm.tasks[i].ID == taskID {
-			tm.tasks[i].Description = description
-			tm.tasks[i].UpdatedAt = time.Now()
-			anyError := tm.saveTasks()
-			if anyError != nil {
-				return nil
-			}
-			return &tm.tasks[i]
-		}
-	}
-	return nil
-}
-
-// return pointer of task by id
-func (tm *TaskManager) ShowTask(taskID int) *Task {
-	for _, task := range tm.tasks {
-		if task.ID == taskID {
-			return &task
-		}
-	}
-	return nil
-}
-
-func (tm *TaskManager) DeleteTask(taskID int) bool {
-	for i := 0; i < len(tm.tasks); i++ {
-		if tm.tasks[i].ID == taskID {
-			tm.tasks = append(tm.tasks[:i], tm.tasks[i+1:]...)
-			tm.saveTasks()
-			return true
-		}
-	}
-	return false
-}
-
-// fajjfarjfjrajfjrjfrjfrjrjfjraj
